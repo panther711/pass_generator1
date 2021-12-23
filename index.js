c76f7ce0f2e020fbec4fa66227e1df9fa56415a3
panther711 <jameshan044@gmail.com>
2021-12-22 20:35:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var generateBtn = document.querySelector("#generate");
+
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+generateBtn.addEventListener("click", writePassword);
