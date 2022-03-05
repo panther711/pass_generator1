b632b55dde1c0216ea817a02ef6b13f4f6ddee89
panther711 <jameshan044@gmail.com>
2022-03-05 16:47:28 -0500

    fucking bugs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,5 @@
 var generateBtn = document.querySelector("#generate");
+
 function generatePassword() {
     //I DEFINED HOW MANY OF THESE CRITERIAS I WILL USE, IF CHOSEN .
     count_of_numeric = 2;
